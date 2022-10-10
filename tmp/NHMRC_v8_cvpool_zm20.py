35b0a3fd2d686f7c3cccea19e0014595a8ea3038
Haifeng Zhao <haifeng.zhao@unimelb.edu.au>
2022-10-11 00:33:07 +1100

    update NHMRC_v8_cvpool_zm20.py to fix bug

--- tmp/NHMRC_v8_cvpool_zm20.py
+++ tmp/NHMRC_v8_cvpool_zm20.py
@@ -564,7 +564,7 @@ def save_combined_tile_burwood_chatswood(BASE_DIR, FROM_FOLDER, N_XY = 5, N_STEP
         print("zoom up conversion finished! - time last: {time.time() - start}")
 
 def save_zoom20_burwood_chatswood():
-    SA2s = [11390, 11398, 11470]
+    SA2s = ['11390', '11398', '11470']
     BASE_DIR = '/data/scratch/projects/punim1358/Datasets/NSW_SA2/age'
     for sa2 in SA2s:
         save_combined_tile_burwood_chatswood(BASE_DIR, sa2, N_XY=2, N_STEP=2, IMGSIZE=256*1, NUM_CPUS=24)
